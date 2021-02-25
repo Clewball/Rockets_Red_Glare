63ee7beb83e170560946b2b44df6b7e0651bc4f7
Clewball <69358475+Clewball@users.noreply.github.com>
2021-02-24 20:54:19 -0700

    new changes
    ch ch ch changes

--- js/site.js
+++ js/site.js
@@ -1,5 +1,6 @@
-var total = 0;
-var item = "";
+let total = 0;
+let item = "";
+let orderno = "asdf";
 
 $("button").click(function() {
     total += Number($(this).val()); 
@@ -11,5 +12,56 @@ $("button").click(function() {
     $("#item").text(item);
 });
 
+/*$("#order").click(function() {
+    orderno = date.now();
+    alert(orderno); 
+    gtag('event','purchase',{
+        "transaction": orderno,
+        "affiliation": "Rockets red glare",
+        "value": total,
+        "currency": "USD",
+        "tax":0.00,
+        "shipping":0.00,
+        "items": [
+            {
+                "id": item,
+                "name": "RRG" + item,
+                "list_name": "Search Results",
+                "brand": "RRG",
+                "category": "rockets",
+                "variant": item,
+                "listposition": 1,
+                "quantity": 1,
+                "price": total
+            }
+        ]
+    });        
+});*/
 
+
+$("#order").click(function() {
+    orderno = Date.now();
+    /*alert(orderno);*/
+    gtag('event', 'purchase', {
+        "transaction_id": orderno,
+        "affiliation": "Rockets Red Glare",
+        "value": total,
+        "currency": "USD",
+        "tax": 0,
+        "shipping": 0,
+        "items": [
+          {
+            "id": item,
+            "name": "RRG Rocket",
+            "list_name": "Search Results",
+            "brand": "RRG",
+            "category": "Rockets",
+            "variant": item,
+            "list_position": 1,
+            "quantity": 1,
+            "price": total
+          }
+        ]
+      }); 
+});
 
